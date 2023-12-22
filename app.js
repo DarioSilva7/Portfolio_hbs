c532ab1ad5752d3aa0911e2af44b7be4ed23ca16
dario <darioemmanuelsilva@gmail.com>
2023-12-22 09:30:28 -0300

    sn-challenge added

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port= 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-const hbs = require('hbs')
+const hbs = require("hbs");
 
-app.set('view engine', 'hbs')
-hbs.registerPartials(__dirname + '/views/partials')
-
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 //sirvo contenido estatico
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('home',{
-        title: 'Portfolio Dario Silva',
-        name: 'Dario Silva'
-    })
-})
+app.get("/", (req, res) => {
+  res.render("home", {
+    title: "Portfolio Dario Silva",
+    name: "Dario Silva",
+  });
+});
 
-app.get('/about',(req,res)=>{
-    res.render('about', {
-        title: 'Portfolio Dario Silva'
-    })
-})
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Portfolio Dario Silva",
+  });
+});
 
-app.get('/proyectos',(req,res)=>{
-    res.render('projects', {
-        title: 'Portfolio Dario Silva'
-    })
-})
+app.get("/proyectos", (req, res) => {
+  res.render("projects", {
+    title: "Portfolio Dario Silva",
+  });
+});
 
-
-app.listen(port, ()=>{
-    console.log(`App listeninng at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`App listeninng at http://localhost:${port}`);
+});
